da48ebb2b6df47463f4497e2ede109cd92b5e11f
Brandon2Black <brandonb627@nycstudents.net>
2026-02-02 12:41:39 -0500

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class GameManager : MonoBehaviour
 {
+
+    public List<GameObject> targets;
     // Start is called before the first frame update
     void Start()
     {
