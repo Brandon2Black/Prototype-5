9544386c42ab28c49624215b34496d8b2aaa078d
Brandon2Black <brandonb627@nycstudents.net>
2026-03-09 12:22:21 -0400

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,9 @@ public class GameManager : MonoBehaviour
     public bool isGameActive;
 
     public Button restartButton;
+
+    public GameObject titleScreen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -62,5 +65,14 @@ public class GameManager : MonoBehaviour
   public void RestartGame()
   {
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+
+  public void StartGame()
+  {
+    isGameActive = true;
+    score = 0;
+    StartCoroutine(SpawnTarget());
+    UpdateScore(0);
+    titleScreen.gameObject.SetActive(false);
   }
 }
